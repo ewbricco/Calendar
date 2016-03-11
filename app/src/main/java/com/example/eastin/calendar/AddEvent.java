ace656ecd71ed2cc07c0e08ff018124b294c77c8
Eastin <ewbricco@gmail.com>
2016-03-11 15:43:22 -0600

    Addevent and eventview are passed all necessary data.

--- app/src/main/java/com/example/eastin/calendar/AddEvent.java
+++ app/src/main/java/com/example/eastin/calendar/AddEvent.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 public class AddEvent extends AppCompatActivity {
 
@@ -13,6 +14,13 @@ public class AddEvent extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+        Bundle extras = getIntent().getExtras();
+        int year = extras.getInt("year");
+        int month = extras.getInt("month");
+        int day = extras.getInt("day");
+        TextView title = (TextView) findViewById(R.id.textView3);
+        title.setText("ON " + month + "/" + day + "/" + year);
+
     }
     public void gotoMain(View view){
         startActivity(new Intent(this, MainActivity.class));
--- app/src/main/java/com/example/eastin/calendar/EventView.java
+++ app/src/main/java/com/example/eastin/calendar/EventView.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class EventView extends AppCompatActivity {
 
@@ -11,6 +12,14 @@ public class EventView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_view);
+
+
+        Bundle extras = getIntent().getExtras();
+        int year = extras.getInt("year");
+        int month = extras.getInt("month");
+        int day = extras.getInt("day");
+        TextView title = (TextView) findViewById(R.id.textView7);
+        title.setText("EVENTS ON " + month + "/" + day + "/" + year);
     }
 
     public void gotoMain(View view){
